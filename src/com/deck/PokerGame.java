0ef4d834e54f12e220edf0591b676cc780e6af14
Greg <gregorypost89@gmail.com>
2020-11-02 08:56:40 -0500

    Added comments and TODOs

--- src/com/deck/PokerGame.java
+++ src/com/deck/PokerGame.java
@@ -15,11 +15,15 @@ public class PokerGame implements CardGames {
     @Override
     public void startGame() {
         System.out.println("Poker started.");
+        // Need to store a deck here from createDeck()
+        // TODO: determine best method to store deck
         List<Card> deck = null;
     }
 
     @Override
     public void enterName() {
+        // Provides input for player name
+        // TODO: Store player name (return, assign)
         Scanner inputName = new Scanner(System.in);
         System.out.println("Enter your name:");
 
@@ -29,6 +33,7 @@ public class PokerGame implements CardGames {
 
     @Override
     public List<Card> createDeck() {
+        // TODO Revisit return statements, test functionality
         List<Card> deck = new ArrayList<>();
         List<String> rankList = Arrays.asList("Ace", "Two", "Three", "Four", "Five", "Six", "Seven",
                 "Eight", "Nine", "Ten", "Jack", "Queen", "King");
@@ -46,22 +51,33 @@ public class PokerGame implements CardGames {
 
     @Override
     public void drawHand(int deckCard) {
+        // Draws set number of cards
+        // Unavailable after starting game (turn 0/1 action only)
+        // TODO: Draw five cards for poker
+        // TODO: Make unavailable after starting game
+        // TODO: Automatic functionality?
         System.out.println("Drawing " + deckCard + " cards from the deck");
         System.out.println("I have " + deckCard + "cards in my hand");
     }
 
     @Override
     public void drawCard() {
+        // Draws a single card
+        // TODO: Implement
         System.out.println("drawCard not implemented");
     }
 
     @Override
     public void endTurn() {
+        // Ends player turn
+        // TODO: Implement
         System.out.println("endTurn not implemented");
     }
 
     @Override
     public void endGame() {
+        // Ends game, returns back to main menu
+        // TODO: Implement
         System.out.println("endGame not implemented");
     }
 }
